ed11acb0ceffb88cc9911e18be44e5ad546d7dc9
Theekshana Jayalath <it23841918@my.sliit.lk>
2026-03-11 20:29:04 +0530

    demo fail test

--- test/step-definitions/applicationSteps.js
+++ test/step-definitions/applicationSteps.js
@@ -44,7 +44,7 @@ When("I send POST request to {string}", async function (endpoint) {
 Then("the response status should be {int}", function (status) {
   console.log("Actual Status:", response.status);
   console.log("Response Body:", response.body);
-   assert.equal(response.status, status);
+  //  assert.equal(response.status, status);
   // Intentional failure for demo
-  // assert.equal(response.status, 999); // 999 is not the real status
+  assert.equal(response.status, 999); // 999 is not the real status
 });
